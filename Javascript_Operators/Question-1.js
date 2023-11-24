8b18a5675c267563942f4c3c8c364b12eb886d87
KrrishNichanii <127929271+KrrishNichanii@users.noreply.github.com>
2023-11-24 18:19:30 +0000

    Operators

---
+++ Javascript_Operators/Question-1.js
@@ -0,0 +1,8 @@
+// Question-1  Explain the usage of ternary operator with syntax and write a program to check whether the number is even
+//             or odd using the ternary operator.
+
+// The ternary operator, also known as the conditional operator, is a concise way to express a conditional (if-else) statement in JavaScript. It is often used as a shorthand for simple decision-making within an expression. 
+
+let num=5;
+(num%2==0)?console.log(`${num} is even`):console.log(`${num} is odd`)
+
---
+++ Javascript_Operators/Question-3.js
@@ -0,0 +1,8 @@
+//Question-3
+
+// Use a nested ternary operator to check that a number is positive, negative or zero. You have to print
+// “positive” if the number is positive and similarly for negative and zero also.
+
+let num=-5;
+
+(num==0)?console.log("Number is zero"):(num>0)?console.log(`${num} is positive`):console.log(`${num} is negative`)
